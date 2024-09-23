517ff761665d6372bfc26c98071fe7e45896a607
Your Name <youremail@domain.com>
2024-09-23 12:10:08 +0300

    do a sentence interpolation

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Tymon'
+
+const welcomeMessage = "Welcome to Flatbook, Tymon!";
+console.log(`${currentUser} ${welcomeMessage}`);
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, TYMON!"
+console.log(`${currentUser} ${excitedWelcomeMessage}`);
+
+const shortGreeting = "Welcome, T!"
+console.log(`${currentUser} ${shortGreeting}`);
